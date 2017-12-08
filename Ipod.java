5e13aca66fa34e2aa01f363c52681d6f2af5ac57
brockdunsie <31520413+brockdunsie@users.noreply.github.com>
2017-12-08 14:29:34 -0700

    LEET

--- Ipod.java
+++ Ipod.java
@@ -8,12 +8,12 @@ public class Ipod {
     public Ipod()
     {
         songlist = new ArrayList<Song>();
-        songlist.add(new Song());
+
     }
 
     public void printSong() {
         for (Song s : songlist) {
-            toString();
+            System.out.println(s.toString());
         }
     }
 
@@ -21,23 +21,83 @@ public class Ipod {
         Scanner scan = new Scanner(System.in);
         String quit = "";
         do {
-            String sname = scan.nextLine();
             System.out.println("Enter the name of the song");
-            String sartist = scan.nextLine();
+            String sname = scan.nextLine();
             System.out.println("Enter the name of the song's artist");
-            String sgenre = scan.nextLine();
+            String sartist = scan.nextLine();
             System.out.println("Enter the song's genre");
-            songlist.add(sname, sartist, sgenre);
-            quit = scan.nextLine();
+            String sgenre = scan.nextLine();
+            songlist.add(new Song(sgenre,  sartist, sname));
             System.out.println("Enter yes to enter another song or Enter no to exit");
+            quit = scan.nextLine();
         }
-        while (quit != "no");
+        while (quit.equalsIgnoreCase("yes"));
     }
 
+    public void find(){
+        Scanner scan = new Scanner(System.in);
+        String quit = "";
+        String t;
+        String a;
+        String g;
+        do {
+
+            System.out.println("Which are you searching by title, artist, or genre?");
+            String f = scan.nextLine();
+            int count = 0;
+            System.out.println("Enter the title");
+            t=scan.nextLine();
+            System.out.println("Enter the artists name");
+            a = scan.nextLine();
+            System.out.println("Enter the genre");
+            g = scan.nextLine();
+            for (Song s : songlist) {
+                if (f.equalsIgnoreCase("title")) {
+                    if(s.getTitle().equals(t))
+                    {
+                        s.toString();
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+
+                }
+                else if (f.equalsIgnoreCase("artist")) {
+
+                    if(s.getArtist().equals(a))
+                    {
+                        s.toString();
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+                else if (f.equalsIgnoreCase("genre")) {
+                    if(s.getGenre().equals(g))
+                    {
+                        s.toString();
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+                if (count == songlist.size()) {
+                    System.out.println("Nothing was found");
+                }
+            }
+            System.out.println("Enter yes to search again or Enter no to exit");
+            quit = scan.nextLine();
+        }while (quit.equalsIgnoreCase("yes"));
+    }
     public static void main(String[] args)
     {
         Ipod song = new Ipod();
         song.addSongs();
         song.printSong();
+        song.find();
+
     }
 }
--- Quadratic.java
+++ Quadratic.java
@@ -1,16 +1,18 @@
-import javax.swing.JLabel;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
-public class Quadratic {
-    String aText = JOptionPane.showInputDialog(null, "Enter a number for a: ");
-    String bText = JOptionPane.showInputDialog(null, "Enter a number for b: ");
-    String cText = JOptionPane.showInputDialog(null, "Enter a number for c: ");
-    int a = Integer.parseInt(aText);
-    int b = Integer.parseInt(bText);
-    int c = Integer.parseInt(cText);
-    double x1 = (-b + Math.sqrt(b * b + 4 * a * c)) / 2;
-    double x2 = (-b - Math.sqrt(b * b + 4 * a * c)) / 2;
-    JFrame win = new JFrame("Quadratic");
-    win.setBounds(250,250)
-}
+//import javax.swing.JLabel;
+//import javax.swing.JFrame;
+//import javax.swing.JOptionPane;
+//
+//public class Quadratic {
+//    String aText = JOptionPane.showInputDialog(null, "Enter a number for a: ");
+//    String bText = JOptionPane.showInputDialog(null, "Enter a number for b: ");
+//    String cText = JOptionPane.showInputDialog(null, "Enter a number for c: ");
+//    int a = Integer.parseInt(aText);
+//    int b = Integer.parseInt(bText);
+//    int c = Integer.parseInt(cText);
+//    double x1 = (-b + Math.sqrt(b * b + 4 * a * c)) / 2;
+//    double x2 = (-b - Math.sqrt(b * b + 4 * a * c)) / 2;
+//    public static void main (String[]args) {
+//        JFrame win = new JFrame("Quadratic");
+//        win.setBounds(250,250)
+//    }
+//}
